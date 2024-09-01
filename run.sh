c4d29c5174893f058fc158916c3dfdfb52dcbc20
dtsudo <dtsudo@users.noreply.github.com>
2024-08-31 22:53:33 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+echo "hello world"
 if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+    echo "is wayland"
     exec zypak-wrapper.sh /app/main/chess-comp-stomp-with-hacks --ozone-platform-hint=auto "$@"  
 else
+    echo "is not wayland"
     exec zypak-wrapper.sh /app/main/chess-comp-stomp-with-hacks "$@"  
 fi
+
+echo "xdgsessiontype is $XDG_SESSION_TYPE hmmmmm"

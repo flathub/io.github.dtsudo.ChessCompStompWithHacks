1ed09d1e451a39e75863bc45d711ad6ae89c1ce5
dtsudo <dtsudo@users.noreply.github.com>
2024-10-03 00:33:13 -0500

    Update to v1.03

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+WAYLAND_SOCKET=${WAYLAND_DISPLAY:-"wayland-0"}
+
+if [[ -e "$XDG_RUNTIME_DIR/${WAYLAND_SOCKET}" ]]; then
+    exec zypak-wrapper.sh /app/main/chess-comp-stomp-with-hacks --ozone-platform-hint=auto "$@"  
+else
+    exec zypak-wrapper.sh /app/main/chess-comp-stomp-with-hacks "$@"  
+fi
